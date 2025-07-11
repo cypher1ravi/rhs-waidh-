cf76036cc92293893bc8ec5f375afcb9b29bc43e
ravindra <ravindrachaubey410@gmail.com>
2025-07-11 23:25:10 +0530

    fix: update robotsTxtTransformer to remove Host line from robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,11 @@ module.exports = {
   siteUrl: 'https://rhswaidhan.com',
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  robotsTxtOptions: {
-    additionalSitemaps: ['https://rhswaidhan.com/sitemap.xml'],
-  }
-}
+  robotsTxtTransformer: async (robotsTxt) => {
+    // Remove Host line manually if present
+    return robotsTxt
+      .split('\n')
+      .filter(line => !line.toLowerCase().startsWith('host:'))
+      .join('\n');
+  },
+};
