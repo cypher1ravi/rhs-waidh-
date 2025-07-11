0617ae542d45bcbb65f519b226d0c02b1bdd5bac
ravindra <ravindrachaubey410@gmail.com>
2025-07-11 23:37:07 +0530

    feat: add robots.txt file and disable robotsTxt generation in sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,6 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://rhswaidhan.com',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   sitemapSize: 5000,
-  robotsTxtTransformer: async (robotsTxt) => {
-    // Remove Host line manually if present
-    return robotsTxt
-      .split('\n')
-      .filter(line => !line.toLowerCase().startsWith('host:'))
-      .join('\n');
-  },
 };
